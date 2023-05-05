f31a2dc510fa19e51b900014c1f8f9bde9150787
benjiramm <benjiramm@gmail.com>
2023-05-05 09:25:20 +0300

    Created auth guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,45 +1,33 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { compare, hash } from 'bcrypt';
-import { CreateUserDto } from 'src/users/dtos/users.create.dto';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
+import { use } from 'passport';
 import { UsersService } from 'src/users/users.service';
+import { LoginUserDto } from './dtos/auth.login-user.dto';
 
 @Injectable()
 export class AuthService {
-    constructor( private readonly usersService: UsersService){}
+    constructor( 
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
+    ){}
 
-    public async register(registrationData: CreateUserDto) {
-        const hashedPassword = await hash(registrationData.password, 10) // encrypt password with 10 salt rounds
-        try {
-            const createdUser = await this.usersService.create({
-                ...registrationData,
-                password: hashedPassword
-            });
-            createdUser.password = undefined;
-            return createdUser;
+    async signIn(loginUserDto: LoginUserDto): Promise<any> {
+        const user = await this.usersService.getUserByUsername(loginUserDto.username)
+        if (!user){
+            // user doesn't exist
+            throw new HttpException('Invalid credentials',HttpStatus.BAD_REQUEST)
         }
-        catch {
-            throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 
-    public async getAuthenticatedUser(loginCredentials : CreateUserDto){
-        try{
-            // find the user that we want to reference
-            const user = await this.usersService.getUserByUsername(loginCredentials.username)
-            await this.verifyPassword(loginCredentials.password, user.password)
-        
-            user.password = undefined;
-            return user;
-        } catch (error) {
-            throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST)
+        const isPasswordMatching = await compare(loginUserDto.password,user.password)
+        if(!isPasswordMatching){
+            // user exists but password is incorrect
+            throw new HttpException('Invalid credentials',HttpStatus.BAD_REQUEST)
         }
-    }
 
-    private async verifyPassword(plainTextPassword :string, hashedPassword:string){
-        const isPasswordMatching = await compare(plainTextPassword,hashedPassword);
-        
-        if (!isPasswordMatching) {
-            throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST)
+        const payload = {username: user.username, sub: user._id, is_admin:user.is_admin}
+        return {
+            access_token: await this.jwtService.signAsync(payload)
         }
     }
 }
