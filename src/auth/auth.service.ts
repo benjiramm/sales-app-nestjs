b6b6642b40e03d59421eb6b2728020ec7f81057a
benjiramm <benjiramm@gmail.com>
2023-08-31 22:51:41 +0300

    added secure attribute

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -50,6 +50,7 @@ export class AuthService {
       expires: new Date(new Date().getTime() + 30 * 1000 * 60), //1 hour
       httpOnly: true,
       sameSite: 'none',
+      secure: true,
       path: '/',
     });
     return response.send('logged in successfully');
