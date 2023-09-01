f1528047731e46bd7a2b6fe92b261db8b7d8c4a5
benjiramm <benjiramm@gmail.com>
2023-09-01 19:45:41 +0300

    removed www

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: 'https://www.sellingcompetition.com',
+    origin: 'https://sellingcompetition.com',
     //'http://localhost:3001'
     credentials: true,
     allowedHeaders: [
