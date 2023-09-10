e213345dab60957cea25c4283caf5cee904b5fd5
benjiramm <benjiramm@gmail.com>
2023-09-10 19:52:17 +0300

    added cors to gateway

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
       ? 'https://www.sellingcompetition.com'
       : 'http://localhost:3001',
     credentials: true,
+
     allowedHeaders: [
       'Origin, X-Requested-With, Content-Type, Accept, Authorization, Set-Cookie,Access-Control-Allow-Origin',
     ],
