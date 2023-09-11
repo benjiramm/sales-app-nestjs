38e4cb2e93ee8f9e68944d16b5e0ad15591edb7c
benjiramm <benjiramm@gmail.com>
2023-09-11 11:35:43 +0300

    fixed

--- src/main.ts
+++ src/main.ts
@@ -24,15 +24,15 @@ async function bootstrap() {
   dotenv.config();
 
   // Setting up swagger
-  // const config = new DocumentBuilder()
-  //   .setTitle('Sales App API')
-  //   .setDescription('the backend of the Sales App')
-  //   .setVersion('1.0')
-  //   .addTag('Sales App NestJS routes')
-  //   .addBearerAuth()
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('/api/docs', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('Sales App API')
+    .setDescription('the backend of the Sales App')
+    .setVersion('1.0')
+    .addTag('Sales App NestJS routes')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
